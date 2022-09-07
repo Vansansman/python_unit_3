a0e7402834d80fa8cf542059c1618bc669cfc877
Vansansman <mamintargetolog@gmail.com>
2022-09-07 21:58:26 +0300

    added test_new

--- test_example.py
+++ test_example.py
@@ -1,9 +1,12 @@
 def test_0():
     assert 2 > 1
-
 def test_1():
     assert 3 >= 2
+def test_2():
+    assert 4 >= 2
+def test_one():
+    assert 10 > 15
+def test_new():
+    assert 2 != 1
 
-def tes_one():
-    asser 10>15
 
